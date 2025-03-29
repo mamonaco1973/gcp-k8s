5d8494520f9058e28bc28340092953b2692496aa
mamonaco1973 <mamonaco1973@gmail.com>
2025-03-29 21:37:18 +0000

    add nat

--- 03-gke/networking.tf
+++ 03-gke/networking.tf
@@ -16,3 +16,17 @@ resource "google_compute_subnetwork" "gke_subnet" {
   region        = var.region                     # Region must match your cluster (e.g., "us-central1")
   network       = google_compute_network.gke_vpc.id  # 🔗 Tie this subnet to the VPC above
 }
+
+resource "google_compute_router" "nat_router" {
+  name    = "gke-nat-router"
+  network = google_compute_network.gke_vpc.name
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat_config" {
+  name                               = "gke-nat"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
