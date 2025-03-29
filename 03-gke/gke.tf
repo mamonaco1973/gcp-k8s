f16fdbd48b26877fb4bcfa34c5b80432bacfc429
mamonaco1973 <mamonaco1973@gmail.com>
2025-03-29 22:28:17 +0000

    make nodes private

--- 03-gke/gke.tf
+++ 03-gke/gke.tf
@@ -61,3 +61,5 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   # 🧾 Ensure TLS verification by providing the cluster's CA cert
 }
+
+
